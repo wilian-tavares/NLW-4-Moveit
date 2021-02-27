b094aaf3d0b9796f7edca38be866a1af33bfa7d2
wilian-tavares <wilian.tavares@souunit.com.br>
2021-02-27 11:42:59 -0300

    minusculas

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
-import { ChallengesContext } from '../Contexts/ChallengesContext';
-import { CountdownContext } from '../Contexts/CountdownsContext';
-import styles from '../Styles/components/ChallengeBox.module.css';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownsContext';
+import styles from '../Styles/Components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
     const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
