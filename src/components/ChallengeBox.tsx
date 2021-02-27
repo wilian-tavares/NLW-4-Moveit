79df6e71f9a683fcfcd6786be5cf97c681ff87bf
wilian-tavares <wilian.tavares@souunit.com.br>
2021-02-26 22:59:40 -0300

    last commit

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -4,65 +4,6 @@ import { CountdownContext } from '../Contexts/CountdownsContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-    const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext)
-    const { resetCountdown } = useContext(CountdownContext);
-
-    function handleChallengeSucceeded() {
-        completeChallenge();
-        resetCountdown();
-    }
-
-    function handleChallengeFailed() {
-        resetChallenge();
-        resetCountdown()
-    }
-
-    return (
-        <div className={styles.challengeboxContainer} >
-            { activeChallenge ? (
-                <div className={styles.challengeboxActive}>
-                    <header>Ganhe {activeChallenge.amount} xp</header>
-
-                    <main>
-                        <img src={`icons/${activeChallenge.type}.svg`} alt="Novo desafio" />
-                        <strong>Novo dessafio</strong>
-                        <p>{activeChallenge.description}</p>
-                    </main>
-
-                    <footer>
-                        <button
-                            className={styles.challengeboxFailedButton}
-                            type='button'
-                            onClick={handleChallengeFailed} >
-                            Falhei
-              </button>
-
-                        <button
-                            className={styles.challengeboxSuccesedButton}
-                            type='button'
-                            onClick={handleChallengeSucceeded}
-                        >Completei</button>
-                    </footer>
-                </div>
-            ) : (
-                    <div className={styles.challengeboxNotActive}>
-                        <strong>Finalize um ciclo para receber um desafio</strong>
-                        <p>
-                            <img src="icons/level-up.svg" alt="Level up" />
-              Avance de level completando os desafios
-            </p>
-                    </div>
-                )}
-        </ div>)
-}
-
-/*
-import { useContext } from 'react';
-import { ChallengesContext } from '../Contexts/ChallengesContext';
-import { CountdownContext } from '../Contexts/CountdownsContext';
-import styles from '../styles/components/ChallengeBox.module.css';
-
-export function ChallengeBox() {
     const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext);
     const { resetCountdown } = useContext(CountdownContext);
 
@@ -116,5 +57,3 @@ export function ChallengeBox() {
         </div>
     )
 }
-
-*/
